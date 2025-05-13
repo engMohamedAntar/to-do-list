1659a3a631a1e17e19b7fa88be6e6f1f44849d8b
MohamedAntar <aboantar852003@gmail.com>
2025-05-13 15:11:07 +0300

    finished the api features class

--- services/factoryHandler.js
+++ services/factoryHandler.js
@@ -1,40 +1,45 @@
-const asyncHandler= require('express-async-handler');
-const ApiFeatures= require('../utils/apiFeatures');
+const asyncHandler = require("express-async-handler");
+const ApiFeatures = require("../utils/apiFeatures");
 exports.getAll = (Model) =>
   asyncHandler(async (req, res) => {
-    const apiFeatures= new ApiFeatures(Model.find(), req.query).filter();
-    const {mongooseQuery, queryObj}= apiFeatures;
-    const documents= await mongooseQuery;
-    console.log(queryObj);
-    
-    res.status(200).json({ status: "success", data: documents }); 
+    const docsCount= await Model.countDocuments();
+    const apiFeatures = new ApiFeatures(Model.find(), req.query)
+      .filter()
+      .sort()
+      .fieldFilter()
+    .search()
+    .paginate(docsCount);
+    const { mongooseQuery, paginationInfo } = apiFeatures;
+    const documents = await mongooseQuery;
+
+    res.status(200).json({ status: "success", paginationInfo, data: documents });
   });
 
 exports.getOne = (Model) =>
-asyncHandler(async (req,res,next)=>{    
-    const document= await Model.findById(req.params.id);    
-    if(!document)
-        return next(new ApiError('Document not found', 404));
-    
-    res.status(201).json({status:'success', data: document});
-});
+  asyncHandler(async (req, res, next) => {
+    const document = await Model.findById(req.params.id);
+    if (!document) return next(new ApiError("Document not found", 404));
 
-exports.createOne= (Model)=>
-    asyncHandler(async (req,res)=>{ 
-    const newDoc= await Model.create(req.body);    
-    res.status(201).json({status:'success', data: newDoc}); 
-});
+    res.status(201).json({ status: "success", data: document });
+  });
 
-exports.updateOne= (Model)=>
-   asyncHandler(async (req,res)=>{
-    const updatedDoc= await Model.findByIdAndUpdate(req.params.id, req.body, {new: true});
-    res.status(200).json({status: 'success', data: updatedDoc});
-});
+exports.createOne = (Model) =>
+  asyncHandler(async (req, res) => {
+    const newDoc = await Model.create(req.body);
+    res.status(201).json({ status: "success", data: newDoc });
+  });
 
-exports.deleteOne= (Model)=>
-asyncHandler(async (req,res)=>{
-    const user= await Model.findByIdAndDelete(req.params.id);
-    if(!user)
-        return new ApiError('no document found', 204);
+exports.updateOne = (Model) =>
+  asyncHandler(async (req, res) => {
+    const updatedDoc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json({ status: "success", data: updatedDoc });
+  });
+
+exports.deleteOne = (Model) =>
+  asyncHandler(async (req, res) => {
+    const user = await Model.findByIdAndDelete(req.params.id);
+    if (!user) return new ApiError("no document found", 204);
     res.status(204).send();
-});
+  });
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,23 +1,77 @@
 class ApiFeatures {
-    constructor(mongooseQuery, queryObj) {
-        this.mongooseQuery= mongooseQuery;
-        this.queryObj= queryObj;
-    }
-    filter() {
-        //1) execlude these words sort, fields, keyword, page, limit from the qeryObj
-        const execluded_queries= ['sort', 'fields', 'keyword', 'page', 'limit'];
-        let queryObj= {...this.queryObj};
-        execluded_queries.forEach(val=>delete queryObj[val]);
+  constructor(mongooseQuery, queryObj) {
+    this.mongooseQuery = mongooseQuery;
+    this.queryObj = queryObj;
+  }
+  filter() {
+    //1) execlude these words sort, fields, keyword, page, limit from the qeryObj
+    const execluded_queries = ["sort", "fields", "keyword", "page", "limit"];
+    let queryObj = { ...this.queryObj };
+    execluded_queries.forEach((val) => delete queryObj[val]);
 
-        //2) add $ before each of these words gte , gt , lte , lt 
-        queryObj= JSON.stringify(queryObj);
-        queryObj= queryObj.replace(/\b(gte|gt|lt|lte)\b/g, (match)=> `$${match}`);
-        
-        this.mongooseQuery= this.mongooseQuery.find(JSON.parse(queryObj));
-        return this;
-    }
+    //2) add $ before each of these words gte , gt , lte , lt
+    queryObj = JSON.stringify(queryObj);
+    queryObj = queryObj.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`);
 
+    this.mongooseQuery = this.mongooseQuery.find(JSON.parse(queryObj));
+    return this;
+  }
+  sort() {
+    if (this.queryObj.sort) {
+      const sortString = this.queryObj.sort.split(",").join(" ");
+      this.mongooseQuery = this.mongooseQuery.sort(sortString);
+    } else {
+      this.mongooseQuery = this.mongooseQuery.sort("createdAt");
+    }
+    return this;
+  }
+  fieldFilter() {
+    if (this.queryObj.fields) {
+      const fields = this.queryObj.fields.split(",").join(" ");
+      this.mongooseQuery = this.mongooseQuery.select(fields);
+    } else {
+      this.mongooseQuery = this.mongooseQuery.select("-__v");
+    }
+    return this;
+  }
+  search() {
+    const searchObj = {};
+    if (this.queryObj.keyword) {
+      searchObj.$or = [{ name: { $regex: this.queryObj.keyword, $options: 'i' } }]; //?
+    }
+    // console.log(searchObj);
     
+    this.mongooseQuery= this.mongooseQuery.find(searchObj);
+    return this;
+  }
+
+  paginate(docsCount) {
+    const page= this.queryObj.page*1 || 1;
+    const limit= this.queryObj.limit*1 || 10;
+    const skip= (page-1) * limit;
+    this.mongooseQuery= this.mongooseQuery.skip(skip).limit(limit);
+
+    const prevPage= page>1 ? page-1: undefined;
+    const nextPage= page*limit< docsCount ? page+1: undefined;
+
+    const paginationInfo= {
+      numberOfPages: Math.ceil(docsCount/limit),
+      prevPage,
+      currentPage: page,
+      nextPage
+    };
+    this.paginationInfo= paginationInfo;
+    return this;
+  }
 }
 
-module.exports= ApiFeatures;
+module.exports = ApiFeatures;
+
+
+//notices
+/*
+this is how searccObj look like 
+    const searchObj = {
+      $or: [{ name: { $regex: "user", $options: "i" } }],
+    };
+*/
